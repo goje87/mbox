51be2e2b3004f9dc2cd81161b52997e3b87b3ab0
Abhilash Goje <me@goje87.com>
2012-05-27 16:23:00 +0530

    adding coming soon page. any address on this project will redirect to this page.

---
+++ error.php
@@ -0,0 +1,3 @@
+<?php
+header("Location: /mbox/index.php");
+?>
--- index.php
+++ index.php
@@ -1,3 +1,34 @@
-<?php
-Crawler::start();
-?>
+<html>
+  <head>
+    <title>Mbox - Goje87</title>
+    <link rel="stylesheet" href="/G87/js/basic.css" />
+    <link rel="stylesheet" href="index.css" />
+  </head>
+  <body>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=268756293222612";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    
+    
+    <img class="logo center" src="./images/logo-web-large.png" alt="Welcome to MBOX" title="Welcome to MBOX"/>
+    <img class="comingSoon" src="./images/coming-soon.png" />
+    <table class="like center">
+      <tr>
+        <td class="like-message">Like MBOX and receive movie updates on your news feed</td>
+        <td class="like-button">
+          <div class="fb-like" data-href="https://www.facebook.com/goje87.mbox" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div>
+        </td>
+      </tr>
+    </table>
+    
+    <div class="facebook-link center">
+      <img src="./images/facebook-icon-small.png" />
+      <a href="http://www.facebook.com/goje87.mbox">facebook.com/goje87.mbox</a>
+    </div>
+  </body>
+</html>
